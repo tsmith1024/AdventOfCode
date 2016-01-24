209a907121ff8db492002ed642b57ed7faf1005a
Taylor Smith <tsmith1024@gmail.com>
2016-01-24 08:55:50 -0600

    finishing up part one getting part two running

--- day2/day2.go
+++ day2/day2.go
@@ -1,15 +1,57 @@
-package day2
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const fileName = "day2_input.txt"
 
 func main() {
-	fmt.Printf("hello, world")
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	totalWrap := 0
+	totalRibbon := 0
+	for scanner.Scan() {
+		dimensions := strings.Split(scanner.Text(), "x")
+		length, width, height := floatDimensions(dimensions)
+		totalWrap += wrappingArea(length, width, height)
+		totalRibbon += packageRibbon(length, width, height)
+	}
+	fmt.Println("Total wrap needed is:", totalWrap)
+	fmt.Println("Total Ribbon is:", totalRibbon)
 }
 
-// WrappingArea calculates the total wrapping needed for a single package
-func WrappingArea(dimensions []int) int {
-	frontArea := dimensions[0] * dimensions[1]
-	sideArea := dimensions[1] * dimensions[2]
-	topArea := dimensions[2] * dimensions[0]
-	return (frontArea + sideArea + topArea) * 2
+// wrappingArea calculates the total wrapping needed for a single package
+func wrappingArea(length float64, width float64, height float64) int {
+	frontArea := length * width
+	sideArea := width * height
+	topArea := height * length
+	bonusArea := math.Min(frontArea, math.Min(sideArea, topArea))
+	return int(2*(frontArea+sideArea+topArea) + bonusArea)
+}
+
+func packageRibbon(length float64, width float64, height float64) int {
+	largestSide := math.Max(length, math.Max(width, height))
+	perimeter := 2 * (length + width + height - largestSide)
+	volume := (length * width * height)
+	return int(perimeter + volume)
+}
+
+func floatDimensions(dimensions []string) (float64, float64, float64) {
+	length, err := strconv.ParseFloat(dimensions[0], 64)
+	width, err := strconv.ParseFloat(dimensions[1], 64)
+	height, err := strconv.ParseFloat(dimensions[2], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return length, width, height
 }
